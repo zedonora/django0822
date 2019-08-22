134646d788620b68cc7b0aa5a39250370a42cf62
zedonora <timeundtime@gmail.com>
2019-08-23 07:19:37 +0900

    class view

--- product/models.py
+++ product/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     name = models.CharField(verbose_name='상품명', max_length=256)
     price = models.IntegerField(verbose_name='상품가격')
     description = models.TextField(verbose_name='상품설명')
-    stuck = models.IntegerField(verbose_name='재고')
+    stock = models.IntegerField(verbose_name='재고')
     register_date = models.DateTimeField(
         verbose_name='등록날짜', auto_now_add=True)
 
